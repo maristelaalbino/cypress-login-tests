93ac8836063c334e929d3546f93a6bcae47bea3a
maristelaalbino <maristela.marques.a@gmail.com>
2026-04-17 17:43:33 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('submeterLogin', (email, senha)=> {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email').type(email)
+    cy.get('#password').type(senha)
+
+    cy.contains('button', 'Entrar').click()
+
+})
